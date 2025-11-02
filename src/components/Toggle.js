8a66c82e057c2e4047d2c8386475a88d43261d0d
Michelle Di Guglielmo <mfdigug@gmail.com>
2025-11-02 21:43:46 +1100

    done with state and events code along

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,16 @@
-import React from "react";
+import { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+
+  const [isOff, setIsOff] = useState(true);
+  const color = isOff ? "white" : "red";
+  function handleClick(){
+     setIsOff(!isOff)
+  }
+
+  return <button style={{ background: color }} onClick={handleClick}>
+    {isOff ? "OFF" : "ON"}
+    </button>;
 }
 
 export default Toggle;
